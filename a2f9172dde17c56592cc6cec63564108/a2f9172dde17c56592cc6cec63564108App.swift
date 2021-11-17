480872b86e24883342abca77f1f4fda93e20bfa5
Kasim Ozdemir <mdykozdemir@thy.com>
2021-11-17 16:46:40 +0300

    Initial Commit

---
+++ a2f9172dde17c56592cc6cec63564108/a2f9172dde17c56592cc6cec63564108App.swift
@@ -0,0 +1,20 @@
+//
+//  a2f9172dde17c56592cc6cec63564108App.swift
+//  a2f9172dde17c56592cc6cec63564108
+//
+//  Created by KasimOzdemir on 17.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct a2f9172dde17c56592cc6cec63564108App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
